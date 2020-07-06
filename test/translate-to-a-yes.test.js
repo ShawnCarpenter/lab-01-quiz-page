ea8167f6fab42e996f6ceaad0d37d98e6da38f93
Shawn Carpenter <shawn.carpeenter@gmail.com>
2020-07-06 13:18:21 -0700

    created translate to a yes function and tests

---
+++ test/translate-to-a-yes.test.js
@@ -0,0 +1,38 @@
+// IMPORT MODULES under test here:
+// import example from '../example.js';
+
+import { translateToAYes } from '../translate-to-a-yes.js'
+
+const test = QUnit.test;
+
+test('translateToAYes should take a string and return true if the string starts with the letter "y"', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const answer = 'yup';
+    const expected = true;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = translateToAYes(answer);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+
+test('translateToAYes should take a string and return false iff the string does not start with the letter "y"', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const answer = 'Nope';
+    const expected = false;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = translateToAYes(answer);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
---
+++ translate-to-a-yes.js
@@ -0,0 +1,5 @@
+export function translateToAYes(answer) {
+    // const firstLetter = answer.charAt(0).toLowerCase();
+    // return (firstLetter === 'y');
+    return answer.charAt(0).toLowerCase() === 'y';
+}
